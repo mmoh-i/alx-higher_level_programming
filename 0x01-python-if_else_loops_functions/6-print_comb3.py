dea0088ca4a9b83e57a670b863c641b474142d04
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-06-29 17:15:46 +0100

    comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(1, 90):
     if i % 10 > i / 10:
         if i != 89:
-            print("{:02d}".format(i),end= ",")
+            print("{:02d}".format(i), end= ", ")
         else:
             print("{:02d}".format(i))
