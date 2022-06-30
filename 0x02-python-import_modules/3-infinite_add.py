9d21f2fa59346e78b21f14c7c623ce419df8a374
mustapha muhammad <ibrahimmusty.me@gmail.com>
2022-06-30 23:49:33 +0000

    infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import sys
+if __name__ == "__main__":
+    from sys import argv
+   # l = len(argv)
+    add = 0
+    for i in argv[1:]:
+        add += int(i)
+        print("{:d}".format(add)) 
