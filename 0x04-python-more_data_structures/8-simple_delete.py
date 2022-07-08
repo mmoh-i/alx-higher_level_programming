05d1dc791de7a62e1f5a35cdea2b172bb2c610b3
mustapha muhammad <ibrahimmusty.me@gmail.com>
2022-07-08 02:44:17 +0000

    delte simply

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key = ""):
-    dic = {}
-    for k, v in a_dictionary.items():
-        if k != key:
-            dic[k] = v
-    return (dic)
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return (a_dictionary)
