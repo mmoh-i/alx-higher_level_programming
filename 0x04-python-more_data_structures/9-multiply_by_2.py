dc789ea1d9e05eb97515966e6c22389a69958471
mustapha muhammad <ibrahimmusty.me@gmail.com>
2022-07-08 02:59:59 +0000

    mulipes

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,4 @@
 #!/ussr/bin/python3
-def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for k, v in a_dictinary.items():
-        new_dict[k] = v * 2
-    return (new_dict)
+def print_sorted_dictionary(a_dictionary):
+    
+    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
