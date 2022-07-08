a4be113e47401c1837df99df6d312af46de2d731
mustapha muhammad <ibrahimmusty.me@gmail.com>
2022-07-08 01:20:03 +0000

    sorted_dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    lis = list(a_dictionary.keys())
+    lis.sort()
+
+    for i in lis:
+        print("{} {}".format(i, a_dictionary.get(i))
