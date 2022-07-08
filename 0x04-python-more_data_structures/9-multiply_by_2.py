c2ef38c0b4ae4b6486996941a2475da3019f3fe5
mustapha muhammad <ibrahimmusty.me@gmail.com>
2022-07-08 10:25:24 +0000

    2_multiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
 #!/ussr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    
-    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        new_dict[key] = value * 2
+    return (new_dict)
