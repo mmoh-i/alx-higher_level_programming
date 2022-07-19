62b9db98647cae7d5f4a324eea25c0b6eb307128
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-07-19 01:21:31 +0100

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/bin/usr/python3
 def safe_print_integer_err(value):
+    import sys
     try:
-        print("{}".format(value)
+        print("{:d}".format(value))
         return True
+        
     except (TypeError, ValueError) as tv:
         print("Exception: {}".format(tv), file=sys.stderr)
         return False
