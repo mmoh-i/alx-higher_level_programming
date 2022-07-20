7f0d19e989d61874a1b665a0ac3d2866b6fb4190
mustapha muhammad <ibrahimmusty.me@gmail.com>
2022-07-20 17:15:18 +0000

    public instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+"""square method"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+
+        """defining the public instance variable size"""
+        self.size = size
+
