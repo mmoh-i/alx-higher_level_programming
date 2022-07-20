a84fb479aa50dbc02457116c11cbdcfe4f28d087
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-07-20 18:33:27 +0100

    private instance

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python3
-"""square module"""
+usr/bin/python3
+"""Square module."""
 
 
 class Square:
-    """Defines a square"""
+    """Defines a square."""
+
     def __init__(self, size):
         """Constructor.
         Args:
             size: length of side of the square.
         """
         self.__size = size
-
