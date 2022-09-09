c390e88a2d7a1cbc4d286954062b1c0be0d36b1b
mmoh-i <ibrahimmusty100@yahoo.com>
2022-09-08 19:43:51 -0700

    sql introductionwq

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- List all records in the table second)Table with a score >= 10.
 SELECCT `score`, `name`
    FROM `second_table`
- WEHERE `score` >= 10
+  WHERE `score` >= 10
 ORDER BY `score` DESC;
