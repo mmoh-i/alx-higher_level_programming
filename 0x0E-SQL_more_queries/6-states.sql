3eced8b6507e11e3f2784e424ff71e1a670af9e2
mmoh-i <ibrahimmusty100@yahoo.com>
2022-09-24 07:48:02 -0700

    sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 --a SCRIPT THAT CREATES THE DATABASS htbn_0d_usa and the table states.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-       `id` INT 	NOT NULL AUTO_INCREMENT,
-       `name` VARCHAR(256) NOT NULL,
-       PRIMARY KEY (`id`)
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+--A script that creates the database htbn_0d_usa with table cities.
+--(in the database hbtn_0d_usa)on MySqL.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.cities (
+	PRIMARY KEY(`id`);
+	FORIENG KEY(`state_id`) REFRENCES `hbtn_0d_usa`.`states`(`id`);
+	`id`       INT	        NOT NULL AUTO_INCREMENT;
+	`state_id` INT	        NOT NULL;
+	`name`     VARCHAR(256) NOT NULL
+);
+
