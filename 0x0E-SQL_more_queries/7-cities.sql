7599b5720dc0a05bb0067527bce4e3ba898cb578
mmoh-i <ibrahimmusty100@yahoo.com>
2022-09-24 08:35:43 -0700

    sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.cities (
 	PRIMARY KEY(`id`),
 	FORIENG KEY(`state_id`) REFRENCES `hbtn_0d_usa`.`states`(`id`),
-	`id`       INT	        NOT NULL AUTO_INCREMENT,
+	`id`       INT	        NOT NULL AUTO_INCREMENT UNIQUE,
 	`state_id` INT	        NOT NULL,
 	`name`     VARCHAR(256) NOT NULL
 );
