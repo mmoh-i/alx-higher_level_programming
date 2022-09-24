c05f0561f209c3e650e4463e115dcf84cc137ad1
mmoh-i <ibrahimmusty100@yahoo.com>
2022-09-24 07:20:47 -0700

    sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 --a SCRIPT THAT CREATES THE DATABASS htbn_0d_usa and the table states.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
        `id` INT 	NOT NULL AUTO_INCREMENT,
        `name` VARCHAR(256) NOT NULL,
