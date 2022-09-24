e52d3072244155a427ddf1f44a3bf3486dda0ef4
mmoh-i <ibrahimmusty100@yahoo.com>
2022-09-24 01:28:50 -0700

    database hbtn_0d_2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+--script that creates the `database hbtn_0d_2 and the user_0d_2.
+CREATE DATABASE
+IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER 
+IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2pwd';
+GRANT SELECT
+ON "."
+TO 'user_0d_2'@'localhost';
+
