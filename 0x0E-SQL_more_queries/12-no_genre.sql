9c800e6403367e759379b612a2d30c2dcfc68a04
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-09-25 16:48:30 +0100

    Create 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Import the database dump from hbtn_0d_tvshows to your MYSQL server.
+-- A script that lists all shows contained in hbn_0d_tvshows without a genre linked.
+SELECT t.`title`, g.`id`
+  FROM `tv_show` AS t
+       LEFT JOIN `v_show_genre` AS g
+       ON t.`id` = g.`show_id`
+       WHERE g.genre_id IS NULL
+ ORDER BY t.`title`, g.genre_id`;
