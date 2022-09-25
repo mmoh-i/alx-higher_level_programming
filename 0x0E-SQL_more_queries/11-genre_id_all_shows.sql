e88763f0d66df9e37afc5b9ae8cc6e607898126f
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-09-25 15:15:50 +0100

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,6 +2,6 @@
 --A script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT t.`title`, g.`genre_id`
   FROM `tv_shows` AS t
-  LEFT JOIN `tv_show_genres` AS g
-    ON t.`id` = g.`show_id`
+       LEFT JOIN `tv_show_genres` AS g
+       ON t.`id` = g.`show_id`
  ORDER BY t.`title`, g.`genre_id`;
