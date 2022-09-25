2a27ae60d38ff0b292b17927a405ac6df2565f11
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-09-25 15:22:20 +0100

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,7 @@
--- Lists all shows contained in the database hbtn_0d_tvshows.
--- Displays NULL for shows without genres.
--- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
-SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s
+--import database dump of hbtn_0d_tvshows to your MYSQL sever.
+--A script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT t.`title`, g.`genre_id`
+  FROM `tv_shows` AS t
        LEFT JOIN `tv_show_genres` AS g
-       ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+       ON t.`id` =  g.`show_id`
+ ORDER BY t.`title`, g.`genre_id`;
