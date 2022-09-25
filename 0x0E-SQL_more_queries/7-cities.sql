255d7651712dc46011d6af5e9b5e6a0c6cd7f3f3
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-09-25 15:55:37 +0100

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,9 +3,10 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.cities (
     PRIMARY KEY(`id`),
-    FORIENG KEY(`state_id`) REFRENCES `hbtn_0d_usa`.`states`(`id`),
     `id`       INT	        NOT NULL AUTO_INCREMENT UNIQUE,
     `state_id` INT	        NOT NULL,
-    `name`     VARCHAR(256)     NOT NULL
+    `name`     VARCHAR(256) NOT NULL,
+    FORIENG KEY(`state_id`)
+    REFRENCES `hbtn_0d_usa`.`states`(`id`)
 );
 
