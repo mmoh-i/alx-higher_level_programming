13f456a2e4ab35c69e3a41d85fb1cd822ac5ca39
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-09-25 16:49:08 +0100

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,7 @@
 -- A script that lists all shows contained in hbn_0d_tvshows without a genre linked.
 SELECT t.`title`, g.`id`
   FROM `tv_show` AS t
-       LEFT JOIN `v_show_genre` AS g
+       LEFT JOIN `tv_show_genre` AS g
        ON t.`id` = g.`show_id`
        WHERE g.genre_id IS NULL
  ORDER BY t.`title`, g.genre_id`;
