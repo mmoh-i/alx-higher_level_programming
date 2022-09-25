5a45eaa490f948c3157a747a3ddcce9f3a6f0d10
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-09-25 15:23:29 +0100

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,6 +2,6 @@
 --A script that lists all shows contained in hbtn_0d_tvshows that have at list one genred linked.
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
-        INNER JOIN `tv_show_genres` AS g
-	ON s.`id` = g.`show_id`
+       INNER JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
  ORDER BY s.`title`, g.`genre_id`;
