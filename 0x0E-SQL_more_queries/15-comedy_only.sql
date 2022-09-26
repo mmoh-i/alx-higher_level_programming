56270513b6e5f65c878e5d197bd22af37be3f508
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-09-26 06:36:42 +0100

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,11 @@
 -- Import database dump form hbtn_0d_tvshows to MySQL.
 -- A script that lists all comedy shows in the database.
 SELECT t.`title`
- FROM  `tv_shows` AS t
-       INNER JOIN  `tv_shows_genres` AS s
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
        ON t.`id` = s.`show_id`
-       
+
        INNER JOIN `tv_genres` AS g
        ON g.`id` = s.`genre_id`
        WHERE g.`name` = "Comedy"
- ORDER BY t.`title`; 
+ ORDER BY t.`title`;
