f04bbee72054513957e78c288fe289d05101e8fe
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-09-26 06:31:35 +0100

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,11 +2,11 @@
 -- A script that lists all comedy shows in the database.
 SELECT t.`title`
  FROM `tv_shows` AS t
-      INNER JOIN `tv_genres` AS g
-      ON t.`id` = g.`id`
-      WHERE g.`name` = 'Comedy'
-      
       INNER JOIN  `tv_shows_genres` AS s
-      ON g.`id` = s.`show_id`
+      ON t.`id` = s.`show_id`
+      
+      INNER JOIN `tv_genres` AS g
+      ON g.`id` = s.`genre_id`
+      WHERE g.`name` = 'Comedy'
 ORDER BY t.`title`
  
