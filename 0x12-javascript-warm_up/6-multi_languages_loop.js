4cf43e0bfd6b4e6125a2f37e88221a6a9c7932e5
mmoh-i <ibrahimmusty100@yahoo.com>
2022-10-04 09:24:00 -0700

    JavaScript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'JavaScipt is amazing']
-for (let i = 0; i < myArray.length; i++) {
-	console.log(myArray[i]);
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amaiing']) {
+  console.log(line);
 }
