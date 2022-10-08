a9a4471dc7361f91d1313b63c12923f35bc1354b
mmoh-i <ibrahimmusty100@yahoo.com>
2022-10-08 08:35:40 -0700

    orm

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Scripts that lists all states with name 
+starting with N(upper N) from the hbtn_0e_0_usa
+database.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
