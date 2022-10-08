e0aeec24fbd27b8f8f844cf99ffc2966e91b0f17
mmoh-i <ibrahimmusty100@yahoo.com>
2022-10-08 08:13:12 -0700

    orm

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Script should take three arguments: mysql usernae
+mysql password, dataase name.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQL.connect(user=sys.argv[1], passwd=sys.agv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states`")
+    [print(state) for state in cur.fetchmall()]
