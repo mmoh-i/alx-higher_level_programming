bd95cd3ba175953c83df5900a5314a30fadad958
mmoh-i <ibrahimmusty100@yahoo.com>
2022-10-08 10:02:03 -0700

    orm

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Scripts that lists all states with name 
-starting with N(upper N) from the hbtn_0e_0_usa
+Scripts that lists all states with name \
+starting with N(upper N) from the hbtn_0e_0_usa\
 database.
 """
 import sys
@@ -10,5 +10,5 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    c.execute("SELECT * FROM states ORDER BY `id`")
     [print(state) for state in c.fetchall() if state[1][0] == "N"]
