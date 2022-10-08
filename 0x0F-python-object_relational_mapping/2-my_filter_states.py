f85e9df69fd4617723a46015ec8ce3083444991d
mmoh-i <ibrahimmusty100@yahoo.com>
2022-10-08 09:48:31 -0700

    orm

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Scriit that takes inan argument and displays
+Script that takes inan argument and displays
 all th value in the states table fo hbtn_0e_p0_usa
 where name matches the argument.
 """
@@ -8,14 +8,14 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQL.connect(user=sys.argv[1], port=3306, host="localhost",
-            passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.exceute("SELECT * FROM states  WHERE name LIKE '{:s}' ORDER BY\
-            ID ASC".format(sys.argv[4]))
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
     states = c.fetchall()
-    for states in states:
-        if states[1] ==  sys.argv[4]:
+    for state in states:
+        if state[1] == sys.argv[4]:
             print(state)
     c.close()
     db.close()
