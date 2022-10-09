1da911c66cbc66cbdead042c6e27bef17cb4a171
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-10-09 12:41:53 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,11 +1,11 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """
 script that takes in the name of a state
 as an argument and lists all cities of that state, 
 using the database hbtn_0e_4_usa
 """
 import sys
-import MySQL
+import MySQLdb
 
 if __name__ == "__main__":
   db = MySQL.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf-8")
