278fbf8aa1716d4b721e4583c2cc49ada60a303d
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-10-10 12:16:57 +0100

    Alchmeyorm

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -5,7 +5,7 @@ object from the database hbtn_0e_6_usa.
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import sys
-from model_state import Base,  State
+from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
