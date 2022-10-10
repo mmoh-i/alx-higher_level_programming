77f607993c7691c3c24ac5057ea0a2e05f7658bc
mmoh-i <ibrahimmusty100@yahoo.com>
2022-10-10 06:59:11 -0700

    Alchemyorm

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,18 +7,16 @@ hbtn_0e_6_usa
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State, Base
-
+from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                            .format(sys.aargv[1], sys.argv[2], sys.argv[3],
-                            pool_pre_ping=engine)
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    found = True
-    states = session.query(State)
+
+    found = False
     for state in session.query(State):
         if state.name == sys.argv[4]:
             print("{}".format(state.id))
