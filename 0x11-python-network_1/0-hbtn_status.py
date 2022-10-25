404211eea8d88d9a57cda5d7c1ebe1743b2b0f91
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-10-25 20:28:40 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import urllib.request
     with urllib.request.Request("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
-        print("Body resppnse:")
+        print("Body response:")
         print("\t - type: {}".format(type(body)))
         print("\t - contrnt: {}".format(body))
         print("\t - utf8 content: {}".format(body.decode("utf-8")))
