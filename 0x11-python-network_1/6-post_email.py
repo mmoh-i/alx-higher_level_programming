b476c7a80a8e5c4ef85f0b59812b01615dc211ef
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-10-26 10:49:34 +0100

    pythonNerwork

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,11 +4,12 @@
 - sends a POST request to the passed URL with the email as a parameter
 - displays the body of the response.
 """
-import sys
-import requests
 
 
 if __name__ == "__main__":
+    import sys
+    import requests
+    
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
 
