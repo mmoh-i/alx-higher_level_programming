34ab8aa3bc2165e2c29a5161141e2ba1f093f35e
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-11-27 16:37:28 +0100

    JQuery

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("DIV#add_item").click(function() {
     $("UL.my_list").append("<li>Items</li>");
   });
-  $("#DIVremve_item").click(function() {
+  $("#DIVremove_item").click(function() {
     const last = $("UL.my_list").children().last();
     if (last) {
       last.remove();
