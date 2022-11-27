a1f8621f34d70f982cbd7307979619648fc71a28
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-11-27 17:47:48 +0100

    JQuery

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("INPUT#language_code").keypress(function(event) {
+    if (event.keycode === 13) {
+      $("INPUT#btn_translate").click();
+    };
+  const = $("INPUT#language_code").val();
+  $.get("https://www.fourtonfish.com/hellosalut/hello/'"+const+"', function(data) {
+    $("INPUT#btn_translate").click(function() {
+      $("DIV#hello).text(const); 
+  }); 
+});
