77128c9bf7edf44d4e5a0f008aa2cbb2aecbd866
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-12-14 13:40:02 +0100

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const fs = require('fs');
 
-if (process.argv.length > 3) {
-  fs.writeFile(process.argv[2], process.argv[3], err => {
-    if (err) {
-      console.log(err);
-    }
-  });
-}
+const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
+});
