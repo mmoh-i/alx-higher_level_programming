7d8247a258aa3f6be72c734188b02a0c4b359c2b
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-12-20 21:30:35 +0100

    Create 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""list the 10 most recent commits in a repository
+first arg : owner of repo
+second arg: name of repo"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    url = 'https://api.github.com/repos/{}/{}/commits'\
+          .format(argv[2], argv[1])
+    response = requests.get(url)
+    commit_list = response.json()
+    for commit in commit_list[:10]:
+        sha = commit['sha']
+        author = commit['commit']['author']['name']
+        print('{}: {}'.format(sha, author))
