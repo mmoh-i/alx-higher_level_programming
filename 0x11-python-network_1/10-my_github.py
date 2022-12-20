b20ebcf8ee3a6bbbd4ef1b17b6b8996e9ad0498d
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-12-20 21:26:12 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-# python script to get my gityhub Api
-import requests
-import sys
+"""script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id"""
+
 if __name__ == "__main__":
-  headers = {"username": sys.argv[1], "Authorization": "token {}".formaa(sys.argv[2])}
-  #payload = {"username":, "password":}
-  url = "https://api.github.com/user/{}".format(sys.argv[1])
-  req = requests.get(url, headers=headers)
-  if req.status_code == 200:
-    response = request.json()
-    if user['login'] == sys.argv[1]:
-      print(user['id'])
-    else:
-      print('None')
-  else:
-    print(None)
+    from requests.auth import HTTPBasicAuth
+    import requests
+    from sys import argv
+    basic = HTTPBasicAuth(argv[1], argv[2])
+    response = requests.get('https://api.github.com/user',
+                            auth=basic)
+    print(response.json().get('id'))
