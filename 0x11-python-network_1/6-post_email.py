a3d7c67bdf7540c7d6f608880a03e1a6cba48a14
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-12-20 21:22:13 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""A  script that:
-- takes in a URL and an email address
-- sends a POST request to the passed URL with the email as a parameter
-- displays the body of the response.
-"""
+"""script that takes in a URL and an email address, sends a POST request to
+the passed URL with the email as a parameter,
+and finally displays the body of the response using requests package"""
 
 
 if __name__ == "__main__":
-    import sys
     import requests
-    
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-
-    req = requests.post(url, data=value)
-    print(req.text)
-    
+    from sys import argv
+    value = {'email': argv[2]}
+    response = requests.post(argv[1], data=value)
+    print(response.text)
