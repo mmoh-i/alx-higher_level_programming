2389baa4bde65027cfbcbcd353deb2a75fcc99ae
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-12-20 21:13:44 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
-#!usr/bin/python3
-# Python script that fetches data
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status"""
 
-if __name__ == "__main__":
-    import urllib.request
-    with urllib.request.Request("https://alx-intranet.hbtn.io/status") as response:
-        body = response.read()
-        print("Body response:")
-        print("\t - type: {}".format(type(body)))
-        print("\t - contrnt: {}".format(body))
-        print("\t - utf8 content: {}".format(body.decode("utf-8")))
+from urllib.request import urlopen
+
+with urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+print('Body response:')
+print('\t- type: {}'.format(type(html)))
+print('\t- content: {}'.format(html))
+print('\t- utf8 content: {}'.format(html.decode('utf-8')))
