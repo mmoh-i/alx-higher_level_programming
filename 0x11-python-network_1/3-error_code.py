cfe99cdccbe4f2571a52327f3990d7ee7dcb6f0a
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-12-20 22:01:18 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays
-the body of the response (decoded in utf-8)."""
+"""Script that sends URL request and prints status code on error,
+or the body on success."""
 
 
 if __name__ == "__main__":
-    from urllib.request import urlopen
-    from urllib.error import HTTPError
-    from sys import argv
+    import urllib.error
+    import urllib.request
+    import sys
+
     try:
-        with urlopen(argv[1]) as response:
-            page = response.read()
-            print(page.decode('utf-8'))
-    except HTTPError as error:
-        print("Error code: {}".format(error.code))
+        with urllib.request.urlopen(sys.argv[1]) as r:
+            print(r.read().decode('utf8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
