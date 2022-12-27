e42764adbc334319a98765b51eaccbd86fe096b0
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2022-12-27 17:37:11 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], function (err, data) {
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
-    console.log(data.toString());
+    process.stdout.write(data);
   }
 });
